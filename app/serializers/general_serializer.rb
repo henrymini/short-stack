638180e0979d843618934207be499b009e6d533d
RWPicklechips <hminicucci@gmail.com>
2019-12-12 13:56:51 -0500

    Macros commit
    added macros to general and user models and serializers

--- app/serializers/general_serializer.rb
+++ app/serializers/general_serializer.rb
@@ -1,4 +1,8 @@
 class GeneralSerializer < ActiveModel::Serializer
-  attributes :id, :name, :age, :family
+  attributes :id, :name, :age, :family, :editable
+  belongs_to :user
   # has_many :legion
+  def editable
+    scope == object.user
+  end
 end
